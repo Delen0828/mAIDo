01311718f4d6a1d6db8b17765ca30a083c0a848b
Delen0828 <63326410+Delen0828@users.noreply.github.com>
2022-06-02 19:44:38 +0800

    main window
    first version of main window @TianmingCao

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget,QDialog
-from Ui_untitled import Ui_Dialog  #导入你写的界面类
+from Ui_main import Ui_Form  
  
  
-class MyMainWindow(QDialog,Ui_Dialog): #这里也要记得改
+class MyMainWindow(QMainWindow,Ui_Form): 
     def __init__(self,parent =None):
         super(MyMainWindow,self).__init__(parent)
         self.setupUi(self)
